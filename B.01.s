0e9d6ffffdb48807b1fc3e912e5f746bc1643323
Mauro Diaz <55920871+DuckHappy@users.noreply.github.com>
2024-09-15 20:59:13 -0300

    Add files via upload

---
+++ B.01.s
@@ -0,0 +1,7 @@
+#B.01 Escriba la instrucción necesaria en Ripes para lograr 
+#que el registro x5 tenga el valor
+#hexadecimal 0x000003FF (note que 3FF se codifica con menos de 12 bits)
+#En el área de registros (seleccionado display type Signed), 
+#luego de ejecutar la instrucción
+.text
+    li x5,0x000003FF
